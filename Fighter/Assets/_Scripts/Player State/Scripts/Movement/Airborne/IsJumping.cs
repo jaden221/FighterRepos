fcb225f10ad74f20b7ecb8d98167989915d595bd
jaden221 <jay513gdd@gmail.com>
2020-06-12 19:21:52 -0400

    Added isTurning
    IsTurning has no functionality yet.
    Just saving before creating sub state machines in animator controller and fully implementing isTurning

---
+++ Fighter/Assets/_Scripts/Player State/Scripts/Movement/Airborne/IsJumping.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project.State
+{
+    [CreateAssetMenu(fileName = "New State", menuName = "States/AbilityData/IsJumping")]
+    public class IsJumping : StateData
+    {
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            if (characterState.characterControl.jump)
+            {
+                animator.SetBool(TransitionParameter.Jump.ToString(), true);
+            }
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+
+        }
+
+    }
+
+}
